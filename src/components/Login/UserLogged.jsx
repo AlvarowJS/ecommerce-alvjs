546c49c1ac751cba7670c0498d9984a7e033f173
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-07-10 23:12:01 -0500

    correciones login

--- src/components/Login/UserLogged.jsx
+++ src/components/Login/UserLogged.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const UserLogged = () => {
+
+  const loggout = () => {
+    localStorage.removeItem('token','')
+  }
   return (
-    <article>
+    <article className='login__screen'>
         <i class='bx bxs-user-check'></i>
         <h2>User Logged</h2>
+        <NavLink to={"/"} onClick={loggout}>
+        <h3>Log out</h3>
+
+        </NavLink>
     </article>
   )
 }
